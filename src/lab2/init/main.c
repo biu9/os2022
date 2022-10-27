196aa84473579e36f9374cde09be47237b6b8c07
biu9 <1756127061@qq.com>
2022-10-27 19:19:35 +0800

    init lab2

---
+++ src/lab2/init/main.c
@@ -0,0 +1,13 @@
+#include "printk.h"
+#include "sbi.h"
+
+extern void test();
+
+int start_kernel() {
+    puti(2022);
+    puts(" Hello RISC-V\n");
+
+    test(); // DO NOT DELETE !!!
+
+	return 0;
+}
